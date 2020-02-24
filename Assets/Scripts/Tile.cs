7f719ec8f6359e8bd1cce0e895c01bbeeb41342c
arsalansul <arsalansul@gmail.com>
2020-02-25 02:13:26 +0700

    add outline

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using cakeslice;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -8,20 +9,11 @@ namespace Assets.Scripts
         public GameObject tile;
         
         private Material material;
+        private Outline outline;
 
         public bool Outline
         {
-            set
-            {
-                if (value)
-                {
-                    SetColor(Color.black);
-                    return;
-                }
-
-                SetColor(Color.white);
-
-            }
+            set => outline.enabled = value;
         }
 
         public Tile(Vector3 pos)
@@ -30,13 +22,13 @@ namespace Assets.Scripts
             tile.transform.position = pos;
             tile.transform.rotation = Quaternion.Euler(90, 0, 0);
             material = tile.transform.GetComponent<Renderer>().material;
+            outline = tile.AddComponent<Outline>();
+            outline.enabled = false;
         }
 
         public void SetColor(Color color)
         {
             material.color = color;
         }
-
-
     }
 }
